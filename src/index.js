bd945e4d03dfe39e5819c483d7eeea99c99b0f85
Mark Mead <itsmarkmead@gmail.com>
2023-03-12 19:31:53 +0000

    Target more elements

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 export default function () {
-  const articleElement = document.querySelector("[data-bionic-reading]");
+  const articleElement = document.querySelector('[data-bionic-reading]')
 
   if (!articleElement) {
-    return;
+    return
   }
 
   const articleElements = [
-    ...articleElement.querySelectorAll("h1, h2, h3, h4, h5, h6, p, li, a"),
-  ];
+    ...articleElement.querySelectorAll(
+      'h1, h2, h3, h4, h5, h6, p, li, a, pre, code, span, em, i, strong, b'
+    ),
+  ]
 
   articleElements.forEach((contentElement) => {
-    contentElement.style.fontWeight = 400;
+    contentElement.style.fontWeight = 400
 
-    const elementText = contentElement.innerText;
-    const elementTextArray = elementText.split(" ");
+    const elementText = contentElement.innerText
+    const elementTextArray = elementText.split(' ')
 
     const elementTextArrayWithBold = elementTextArray.map((textWord) => {
-      const wordLength = textWord.length;
+      const wordLength = textWord.length
 
       if (wordLength === 1) {
-        return `<strong>${textWord}</strong>`;
+        return `<strong>${textWord}</strong>`
       }
 
-      const wordLengthHalf = Math.floor(wordLength / 2);
+      const wordLengthHalf = Math.floor(wordLength / 2)
 
-      const wordArray = textWord.split("");
+      const wordArray = textWord.split('')
       const wordArrayWithBold = wordArray.map((wordLetter, letterIndex) => {
         return letterIndex < wordLengthHalf
           ? `<strong>${wordLetter}</strong>`
-          : wordLetter;
-      });
+          : wordLetter
+      })
 
-      return wordArrayWithBold.join("");
-    });
+      return wordArrayWithBold.join('')
+    })
 
-    contentElement.innerHTML = elementTextArrayWithBold.join(" ");
-  });
+    contentElement.innerHTML = elementTextArrayWithBold.join(' ')
+  })
 }
